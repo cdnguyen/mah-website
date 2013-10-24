7772038803697ad4243b55f05b4870b03259e30a
Chanh Nguyen <cnguyen@leadformance.com>
2013-10-24 12:49:00 +0200

    pushed mah website to git yo

---
+++ js/other-scripts.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+  $('.popup-link').magnificPopup({ 
+    type: 'image'
+  });
+
+  $('.popup-video').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+});
